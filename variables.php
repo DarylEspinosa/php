459ba1bd454074da1f136a4799d93d26a04a0928
DarylEspinosa <113522048+DarylEspinosa@users.noreply.github.com>
2022-10-13 08:59:12 -0500

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,16 @@
+<?php
+    // Ejemplos de nombres de variables
+    
+    $edad = 16;
+    $edad1 = 17;
+    $edad_1 = 18;
+
+    // Ejemplos de nombres de varianles (estilos NO recomendables)
+    
+    $años = 19;
+    $_edad = 20;
+    $Edad = 21;
+    $__edad = 22;
+    $EDAD = 23;
+    $_EDAD = 24;
+?>
